1bdd03678e7617efcbc3cc0dd32d3a6602a929ef
AdamJovanovic <adamjovanovix@gmail.com>
2022-10-10 08:16:15 +1100

    Completed TDD-Ex02.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let repeated = [];
+  if (num < 0) {
+    return "ERROR";
+  } else if (num == 0) {
+    return "";
+  } else {
+    for (let i = 0; i < num; i++) {
+      repeated += str;
+    }
+  }
+  return repeated;
 };
 
 // Do not edit below this line
