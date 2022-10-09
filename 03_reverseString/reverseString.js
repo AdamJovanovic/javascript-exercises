5aea2784b710f5bbc977f907e92603763281ee50
AdamJovanovic <adamjovanovix@gmail.com>
2022-10-10 08:20:22 +1100

    Completed TDD-Ex03. Review this in future without loops.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let letters = str.split("");
+  let reverseStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseStr += letters[i];
+  }
+  return reverseStr;
 };
 
 // Do not edit below this line
