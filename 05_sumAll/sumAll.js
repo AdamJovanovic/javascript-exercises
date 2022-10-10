468b485f936997f70cef3cdeadd8054c114caf5d
AdamJovanovic <adamjovanovix@gmail.com>
2022-10-10 19:50:23 +1100

    Completed TDD-Ex05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (numA, numB) {
+  let sum = 0;
+  let first, second;
+  if (
+    numA < 0 ||
+    numB < 0 ||
+    !Number.isFinite(numA) ||
+    !Number.isFinite(numB)
+  ) {
+    return "ERROR";
+  }
+  if (numA > numB) {
+    first = numB;
+    second = numA;
+  } else {
+    first = numA;
+    second = numB;
+  }
+  for (let iterator = first; iterator <= second; iterator++) {
+    sum += iterator;
+  }
+  return sum;
 };
 
 // Do not edit below this line
