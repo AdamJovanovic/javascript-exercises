3c4c213ca5b8482314ce0550a08e2cdf9a56201c
AdamJovanovic <adamjovanovix@gmail.com>
2022-10-10 19:37:10 +1100

    Completed TDD-Ex04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
-
+const removeFromArray = function () {
+  const numRemovals = arguments.length - 1;
+  if (!numRemovals) {
+    return arguments[0];
+  } else {
+    let target = arguments[0];
+    let remove = Array.from(arguments).slice(1, arguments.length);
+    console.log(remove);
+    target = target.filter(function (item) {
+      for (let i = 0; i < remove.length; i++) {
+        if (remove[i] === item) {
+          return false;
+        }
+      }
+      return true;
+    });
+    return target;
+  }
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
