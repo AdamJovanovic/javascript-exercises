33e104b07027202ffa6bc0250fbe201dbb1a5cd2
AdamJovanovic <adamjovanovix@gmail.com>
2022-10-11 21:41:42 +1100

    Completed TDD-Ex07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  return roundOne((fahrenheit - 32) * (5 / 9));
 };
 
-const ctof = function() {
+const ctof = function (celsius) {
+  return roundOne(celsius * (9 / 5) + 32);
+};
 
+const roundOne = function (num) {
+  return Math.round(num * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
