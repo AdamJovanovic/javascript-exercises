7cf5dd8aa5b88bc101dd4c51559b87269d2b73cd
AdamJovanovic <adamjovanovix@gmail.com>
2022-10-11 21:27:36 +1100

    Completed TDD-Ex06.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  //if non-numeric year
+  //    return "ERROR"
+  //if year divisible by 4
+  //  check if divisible by 100 and NOT 400
+  //      return FALSE
+  //  else
+  //      return TRUE
+  //else
+  //  return FALSE
+  //if (!(year % 4)) {
+  if (!(year % 4) && (year % 100 || !(year % 400))) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
